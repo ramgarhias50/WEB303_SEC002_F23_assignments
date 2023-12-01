5bf96c73301527287152a8afa7a725ddf112c2c0
Gursimran Singh Assi <gursimransinghassi@gmail.com>
2023-12-01 17:07:01 -0500

    Add files via upload

---
+++ WEB303As10/js/script.js
@@ -0,0 +1,32 @@
+$(document).ready(function () {
+  countries.forEach((element) => {
+    var option = `<option value=${element.code}>${element.name}</option>`;
+    $("#country").append(option);
+  });
+
+  formValidate();
+});
+
+function formValidate() {
+  var username = $("#username").val();
+  var country = $("#country").val();
+  var password = $("#password").val();
+  var confirmPassword = $("#confirmPassword").val();
+  var termsCheckbox = $("#termsCheckbox").prop("checked");
+
+  if (
+    password.length >= 2 &&
+    password == confirmPassword &&
+    username &&
+    termsCheckbox &&
+    country != ""
+  ) {
+    $("#submitButton").removeAttr("disabled");
+  }
+  $("#submitButton").on("click", function (e) {
+    e.preventDefault();
+    let msg = `Welcome ${username}! The country code you selected is ${country}`;
+    $("#welcomeMessage").text(msg);
+    $("#welcomeMessage").css("display", "block");
+  });
+}
